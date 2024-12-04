88c08655cdb155b4bb18329e67e8797299f7e508
GoLab18 <kuba1golab@gmail.com>
2024-12-04 18:48:15 +0100

    Day 4 part2

--- AOC-2024/Day4/main.go
+++ AOC-2024/Day4/main.go
@@ -17,10 +17,10 @@ var directions = [8][2]int{
 	{0, -1},
 	{1, 0},
 	{-1, 0},
-	{1, 1},
 	{-1, -1},
-	{1, -1},
+	{1, 1},
 	{-1, 1},
+	{1, -1},
 }
 
 func main() {
@@ -47,9 +47,11 @@ func main() {
 		os.Exit(1)
 	}
 
-	count := firstPartSearch(grid)
+	c1 := firstPartSearch(grid)
+	c2 := secondPartSearch(grid)
 
-	fmt.Println("Total occurrences of XMAS:", count)
+	fmt.Println("Total occurrences of XMAS part 1 ->", c1)
+	fmt.Println("Total occurrences of XMAS part 2 ->", c2)
 }
 
 func firstPartSearch(grid []string) (count int) {
@@ -90,4 +92,49 @@ func firstPartSearch(grid []string) (count int) {
 	}
 
 	return
+}
+
+func secondPartSearch(grid []string) (count int) {
+	rows := len(grid)
+	cols := len(grid[0])
+
+	for row := 0; row < rows; row++ {
+		for col := 0; col < cols; col++ {
+			if isXMASCell(grid, row, col) {
+				count++
+			}
+		}
+	}
+
+	return
+}
+
+func isXMASCell(grid []string, row, col int) bool {
+	rows := len(grid)
+	cols := len(grid[0])
+
+	if grid[row][col] != 'A' || row == 0 || col == 0 || row == rows-1 || col == cols-1 {
+		return false
+	}
+
+	var state rune
+	for i := 4; i < len(directions); i++ {
+		dy, dx := directions[i][0], directions[i][1]
+		newRow := row + dy
+		newCol := col + dx
+
+		r := rune(grid[newRow][newCol])
+
+		if r != 'M' && r != 'S' {
+			return false
+		}
+
+		if (i == 5 || i == 7) && state == r {
+			return false
+		}
+
+		state = r
+	}
+
+	return true
 }
