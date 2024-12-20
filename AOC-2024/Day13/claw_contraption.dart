9d06e67df5d4959f6467b1675c61e3fbee22bed0
GoLab18 <kuba1golab@gmail.com>
2024-12-20 17:52:16 +0100

    Day13 part2

--- AOC-2024/Day13/claw_contraption.dart
+++ AOC-2024/Day13/claw_contraption.dart
@@ -5,10 +5,11 @@ Future<void> main(List<String> args) async {
   final filepath = args.isNotEmpty ? args[0] : "test.txt";
   List<ClawMachine> clawMachs = await readFile(filepath);
   
-  int r = minimumTokensToWin(clawMachs);
-
   // Part 1
-  print("Minimum tokens required -> $r");
+  print("Min tokens required -> ${minTokens(clawMachs)}");
+
+  // Part 2
+  print("Unbound min tokens required -> ${minTokensUnbound(clawMachs)}");
 }
 
 Future<List<ClawMachine>> readFile(String filepath) async {
@@ -43,17 +44,36 @@ Future<List<ClawMachine>> readFile(String filepath) async {
 }
 
 class ClawMachine {
-  final int ax, ay, bx, by, px, py;
+  final int ax, ay, bx, by;
+  int px, py;
 
   ClawMachine(this.ax, this.ay, this.bx, this.by, this.px, this.py);
+
+  bool isSolvable() => (px % gcd(ax, bx) == 0) && (py % gcd(ay, by) == 0);
+
+  // Euclid's algo
+  int gcd(int a, int b) => b == 0 ? a : gcd(b, a % b);
+
+  int cramerCost() {
+    int det = ax * by - ay * bx;
+
+    if (det == 0) return -1;
+
+    int detX = px * by - py * bx;
+    int detY = ax * py - px * ay;
+
+    if (detX % det != 0 || detY % det != 0) return -1;
+
+    int a = detX ~/ det;
+    int b = detY ~/ det;
+
+    if (a < 0 || b < 0) return -1;
+
+    return 3*a + 1*b;
+  }
 }
 
-bool isSolvable(int ax, int bx, int px) => px % gcd(ax, bx) == 0;
-
-// Euclid's algo
-int gcd(int a, int b) => b == 0 ? a : gcd(b, a % b);
-
-int minimumTokensToWin(List<ClawMachine> machines) {
+int minTokens(List<ClawMachine> machines) {
   int maxPresses = 100;
   int totalTokens = 0;
 
@@ -61,7 +81,7 @@ int minimumTokensToWin(List<ClawMachine> machines) {
     int ax = mch.ax, ay = mch.ay, bx = mch.bx, by = mch.by;
     int px = mch.px, py = mch.py;
 
-    if (!isSolvable(ax, bx, px) || !isSolvable(ay, by, py)) continue;
+    if (!mch.isSolvable()) continue;
 
     int? minCost;
     for (int a = 0; a <= maxPresses; a++) {
@@ -76,6 +96,22 @@ int minimumTokensToWin(List<ClawMachine> machines) {
     }
 
     if (minCost != null) totalTokens += minCost;
+  }
+
+  return totalTokens;
+}
+
+int minTokensUnbound(List<ClawMachine> machines) {
+  int totalTokens = 0;
+
+  for (var mch in machines) {
+    mch.px += 10000000000000;
+    mch.py += 10000000000000;
+
+    if (!mch.isSolvable()) continue;
+
+    int cost = mch.cramerCost();
+    if (cost != -1) totalTokens += cost;
   }
 
   return totalTokens;
